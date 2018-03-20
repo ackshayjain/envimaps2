94783a3d4aea5ac777e712d47039376c2f19bc0f
sj <jindalsaurabh533@gmail.com>
2018-03-20 11:47:28 +0530

    Add files via upload

--- comp/forms.py
+++ comp/forms.py
@@ -6,10 +6,20 @@ from django import forms
 class AddForm(forms.Form):
 
 
-	title = forms.CharField(max_length=200)
-	desc = forms.CharField(max_length=400)
-	location = forms.CharField(max_length = 100)
-	pic = forms.ImageField()
+
+	title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class':'form-control'}))
+	location = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'class':'form-control'}))
+	desc = forms.CharField(max_length=400, widget=forms.TextInput(attrs={'class':'form-control'}))
+	pic = forms.ImageField(label="Upload Image")
+	# name.widget.attrs.update({'class': 'special'})
+	pic.widget.attrs.update({'class':'form-control'})
+	# myfield = forms.CharField(widget=forms.TextInput(attrs={'class' : 'myfieldclass'}))
+
+	# title = forms.CharField(max_length=200)
+	# desc = forms.CharField(max_length=400)
+	# location = forms.CharField(max_length = 100)
+	# pic = forms.ImageField()
+
 
 	# date_published = forms.DateTimeField(auto_now_add = True)
 	# date_modified = forms.DateTimeField(auto_now = True)
